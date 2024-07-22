4db3ddb6fb5e98477c2143682edb5c29c9ed97cc
Ahmad Sofyan <sofyan@btech.id>
2024-07-22 15:53:28 +0700

    changes on 'double.c'

--- C/double.c
+++ C/double.c
@@ -1,13 +1,15 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int sequence[5];
+    int size = get_int("Enter a size: ");
+    int sequence[size];
 
     sequence[0] = 1;
     printf("%i\n", sequence[0]);
 
-    for (int i = 1; i < 5; i++)
+    for (int i = 1; i < size; i++)
     {
         sequence[i] = sequence[i - 1] * 2;
         printf("%i\n", sequence[i]);
